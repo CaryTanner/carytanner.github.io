61b7a98b6fd735872953294801e0b021d6e1380e
CaryTanner <cary.landon@gmail.com>
2020-12-06 18:11:29 +0100

    landing finished except 600px

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -13,18 +13,17 @@ import {
   Fab,
   Grow,
   Menu,
-  MenuItem
+  MenuItem,
 } from "@material-ui/core";
 
 import MenuIcon from "@material-ui/icons/Menu";
 import styles from "./index.module.css";
 import LaunchIcon from "@material-ui/icons/Launch";
 
-
 function Header() {
-  const trigger = useScrollTrigger({ disableHysteresis: true, threshold: 200});
+  const trigger = useScrollTrigger({ disableHysteresis: true, threshold: 200 });
   const isXs = useMediaQuery((theme) => theme.breakpoints.down("xs"));
-  
+
   //hamburger logic
   const [anchorEl, setAnchorEl] = React.useState(null);
 
@@ -36,25 +35,24 @@ function Header() {
     setAnchorEl(null);
   };
 
-
- // tanner logo designs 
- const logoProps ={
-   border: 4,
-   p: .5,
-   fontFamily: "roboto mono, monospace",
-   lineHeight: .8,
-   letterSpacing: -2,
-   fontWeight: "fontWeightBold",
-   fontSize: "30px"
- }
-
+  // tanner logo designs
+  const logoProps = {
+    border: 4,
+    p: 0.5,
+    fontFamily: "roboto mono, monospace",
+    lineHeight: 0.8,
+    letterSpacing: -2,
+    fontWeight: "fontWeightBold",
+    fontSize: "30px",
+  };
 
   return (
     <>
-      <Box {...logoProps} className={styles.logo}>TAN <Box style={{transform: "rotate(180deg) translateX(-2px)"}}>NER</Box></Box>
-      
-      
-      
+      <Box {...logoProps} className={styles.logo}>
+        TAN{" "}
+        <Box style={{ transform: "rotate(180deg) translateX(-2px)" }}>NER</Box>
+      </Box>
+
       <Grid
         style={{ padding: "2rem 2rem 0 2rem" }}
         container
@@ -70,78 +68,79 @@ function Header() {
           alignItems="flex-end"
         >
           <Hidden xsDown>
-          <Grid
-            item
-            sm={6}
-            className={styles.landingTextBox}
-            container
-            justify="space-between"
-            
-          >
-            <Typography variant="body1">
-              <Box
-                fontWeight="fontWeightBold"
-                lineHeight={0.8}
-                letterSpacing={-1}
-                display="inline"
-              >
-                START
-              </Box>
-            </Typography>
-            <Typography variant="body1">
-              <Box
-                fontWeight="fontWeightMedium"
-                lineHeight={0.8}
-                letterSpacing={-1}
-                display="inline"
-              >
-                ABOUT
-              </Box>
-            </Typography>
-            <Typography variant="body1">
-              <Box
-                fontWeight="fontWeightMedium"
-                lineHeight={0.8}
-                letterSpacing={-1}
-                display="inline"
-              >
-                PROJECTS
-              </Box>
-            </Typography>
-            <Typography variant="body1">
-              <Box
-                fontWeight="fontWeightMedium"
-                lineHeight={0.8}
-                letterSpacing={-1}
-                display="inline"
-                mr={{  lg: 5, md: 5, sm: 1 }}
-              >
-                <LaunchIcon fontSize="inherit" /> CV
-              </Box>
-            </Typography>
-          </Grid>
+            <Grid
+              item
+              sm={6}
+              className={styles.landingTextBox}
+              container
+              justify="space-between"
+            >
+              <Typography variant="body1">
+                <Box
+                  fontWeight="fontWeightBold"
+                  lineHeight={0.8}
+                  letterSpacing={-1}
+                  display="inline"
+                >
+                  START
+                </Box>
+              </Typography>
+              <Typography variant="body1">
+                <Box
+                  fontWeight="fontWeightMedium"
+                  lineHeight={0.8}
+                  letterSpacing={-1}
+                  display="inline"
+                >
+                  ABOUT
+                </Box>
+              </Typography>
+              <Typography variant="body1">
+                <Box
+                  fontWeight="fontWeightMedium"
+                  lineHeight={0.8}
+                  letterSpacing={-1}
+                  display="inline"
+                >
+                  PROJECTS
+                </Box>
+              </Typography>
+              <Typography variant="body1">
+                <Box
+                  fontWeight="fontWeightMedium"
+                  lineHeight={0.8}
+                  letterSpacing={-1}
+                  display="inline"
+                  mr={{sm: 1, md: 5 }}
+                >
+                  <LaunchIcon fontSize="inherit" /> CV
+                </Box>
+              </Typography>
+            </Grid>
           </Hidden>
         </Grid>
-        <Grid item sm={6} container justify="flex-end" >
+        <Grid item sm={6} container justify="flex-end">
           <Hidden xsDown>
-          <Button variant="contained" color={isXs ? 'primary' : 'secondary'} size={isXs ? 'small' : 'medium'}>
-            {" "}
-            contact
-          </Button>
+            <Button
+              variant="contained"
+              color={isXs ? "primary" : "secondary"}
+              size={isXs ? "small" : "medium"}
+            >
+              {" "}
+              contact
+            </Button>
           </Hidden>
           <Hidden smUp>
-            <Button variant="outlined"  onClick={handleClick}><MenuIcon /></Button>
+            <Button variant="outlined" onClick={handleClick}>
+              <MenuIcon />
+            </Button>
           </Hidden>
         </Grid>
       </Grid>
       <Grow in={trigger}>
-      <Fab className={styles.fab} color="primary" onClick={handleClick} >
-        <MenuIcon /> 
-      </Fab> 
-
-        
-        
-      
+        <Fab className={styles.fab} color="primary" onClick={handleClick}>
+          <MenuIcon />
+        </Fab>
       </Grow>
       <Menu
         id="simple-menu"
@@ -153,8 +152,15 @@ function Header() {
         <MenuItem onClick={handleClose}>Start</MenuItem>
         <MenuItem onClick={handleClose}>About</MenuItem>
         <MenuItem onClick={handleClose}>Projects</MenuItem>
-        <MenuItem onClick={handleClose}><LaunchIcon fontSize="inherit" />&nbsp;CV</MenuItem>
-        <MenuItem onClick={handleClose}><Button variant="outlined" color="primary">Contact</Button></MenuItem>
+        <MenuItem onClick={handleClose}>
+          <LaunchIcon fontSize="inherit" />
+          &nbsp;CV
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Button variant="outlined" color="primary">
+            Contact
+          </Button>
+        </MenuItem>
       </Menu>
     </>
   );
--- src/Components/Landing/index.js
+++ src/Components/Landing/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./index.module.css";
 import {
   Container,
@@ -10,25 +10,26 @@ import {
   Button,
   useMediaQuery,
   useScrollTrigger,
-  Fade
+  Fade,
 } from "@material-ui/core";
-import codeBoxGroup from "../../images/codeBoxGroup.svg"
+import codeBoxGroup from "../../images/codeBoxGroup.svg";
 
 export default function Landing() {
-  const isMobile = useMediaQuery(theme => theme.breakpoints.down('sm'));
-  const isXs = useMediaQuery(theme => theme.breakpoints.down('xs'));
-  const [fadeIn, setFadeIn] = useState(false)
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  const isXs = useMediaQuery((theme) => theme.breakpoints.down("xs"));
+  const [fadeIn, setFadeIn] = useState(false);
 
- useEffect(()=>{
-   return setFadeIn(true) 
-    
- }, [])
+  useEffect(() => {
+    return setFadeIn(true);
+  }, []);
 
   return (
     <>
-
-      <Grid container  className={styles.container} style={{padding: "0 2rem"}}>
-
+      <Grid
+        container
+        className={styles.container}
+        style={{ padding: "0 2rem" }}
+      >
         <Grid
           item
           sm={6}
@@ -36,56 +37,70 @@ export default function Landing() {
           className={styles.photoSide}
           container
           direction="column"
-
-          justify="center"
           alignItems={isXs ? "center" : "flex-end"}
-
         >
-          <Grid item  sm={6} className={styles.landingTextBox} >
-
-          <Fade in={fadeIn} timeout={1000}>
-            <Typography variant="h3" >
-              <Box
-                fontWeight="fontWeightMedium"
-                lineHeight={0.8}
-                letterSpacing={-1}
-                
+          <Grid item className={styles.landingTextBox}>
+            <Fade in={fadeIn} timeout={1000}>
+              <Typography variant="h3">
+                <Box
+                  fontWeight="fontWeightMedium"
+                  lineHeight={0.8}
+                  letterSpacing={-1}
+                  mt="220px"
+                >
+                  Hi, I'm Cary
+                </Box>
+              </Typography>
+            </Fade>
+            <Fade
+              in={fadeIn}
+              timeout={1000}
+              style={{ transitionDelay: fadeIn ? "900ms" : "0ms" }}
+            >
+              <Typography variant="h4">
+                <Box
+                  fontWeight="fontWeightLight"
+                  letterSpacing={-2}
+                  my={{ xs: "3rem", sm: "3vw" }}
+                  mr={5}
+                  lineHeight={0.8}
+                >
+                  I can help you build a better website.
+                </Box>
+              </Typography>
+            </Fade>
+            <Fade
+              in={fadeIn}
+              timeout={1000}
+              style={{ transitionDelay: fadeIn ? "1800ms" : "0ms" }}
+            >
+              <Button
+                size={isMobile ? "small" : "medium"}
+                variant="contained"
+                color="primary"
               >
-                Hi, I'm Cary
-              </Box>
+                Work with me
+              </Button>
+            </Fade>
+            <Hidden xsDown>
+              <Typography variant="subtitle2" style={{marginTop: "200px"}}>
+                <Box display="inline" mr={3}>GitHub</Box>
+                <Box display="inline" mr={3}>LinkedIn</Box>
+                <Box display="inline">Email</Box>
+              </Typography>
+            </Hidden>
+          </Grid>
 
-            </Typography>
-            </Fade>
-            <Fade in={fadeIn} timeout={1000} style={{ transitionDelay: fadeIn ? '900ms' : '0ms' }}>
-            <Typography variant="h4">
-              <Box
-                fontWeight="fontWeightLight"
-                letterSpacing={-2}
-                my={{xs: "3rem",sm: "3.5vw" }}
-                lineHeight={0.8}
-              >
-                I can help you build a better website. 
-              </Box>
-            </Typography>
-            </Fade>
-            <Fade in={fadeIn} timeout={1000} style={{ transitionDelay: fadeIn ? '1800ms' : '0ms' }}>
-            <Button size={isMobile ? "small" : "medium"} variant="contained" color="primary">
-              Work with me
-            </Button>
-            </Fade>
+          <Hidden smUp>
+            <Grid item>
+              <img
+                src={codeBoxGroup}
+                className={styles.codeSide}
+                alt="Box of sample code"
+              ></img>
+            </Grid>
+          </Hidden>
         </Grid>
-        <Hidden smUp>
-        <Grid item>
-           <img src={codeBoxGroup} className={styles.codeSide} alt="Box of sample code"></img>
-           </Grid>
-        </Hidden>
-
-
-
-
-        </Grid>
-
-
       </Grid>
     </>
   );
